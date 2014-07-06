42da3b7aa63d105da669e22cf76061c264527cd7
Luiz Fernando Rodrigues <fernahh@gmail.com>
2014-07-06 17:50:34 -0300

    Improved shopping cart. #13

--- src/br/ucs/shopping/ejb/intf/ShoppingCartIntf.java
+++ src/br/ucs/shopping/ejb/intf/ShoppingCartIntf.java
@@ -2,13 +2,16 @@ package br.ucs.shopping.ejb.intf;
 
 import br.ucs.shopping.models.CreditCard;
 import br.ucs.shopping.models.Customer;
+import br.ucs.shopping.models.Item;
 import br.ucs.shopping.models.Product;
 import java.util.List;
 import javax.ejb.Local;
 
 @Local
 public interface ShoppingCartIntf {
-    public void addProduct(Product product, Integer amount);
+    public List<Item> getItems();
     public boolean checkout(Customer customer, CreditCard creditCard);
+    public void addProduct(Product product, Integer amount);
     public void removeAllProducts();
+    public void setItems(List<Item> items);
 }
